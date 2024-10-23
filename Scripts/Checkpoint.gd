0b2399064f0d4d0b54b1a10f367ed49a33ba97d1
RonTheHuman <1ron.kokotov@gmail.com>
2024-10-23 23:05:00 +0300

    added checkpoint particles

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -4,6 +4,7 @@ extends Area2D
 func _on_body_entered(_body):
 	$Sprite2D.region_rect.position.x = 576
 	$PointLight2D.enabled = true
+	$CPUParticles2D.emitting = true
 	get_parent().get_parent().save_game(self)
 
 
