c5721f021a7d6598a90548a4ad14e6b1a7db3f58
RonTheHuman <1ron.kokotov@gmail.com>
2024-10-23 13:48:26 +0300

    Added saving and loading to file

--- Scenes/Dialogues/Dialogue.gd
+++ Scenes/Dialogues/Dialogue.gd
@@ -5,8 +5,10 @@ var D1
 var D2
 
 func _ready():
-	var dia_text = FileAccess.open("Scenes/Dialogues/D1.txt", FileAccess.READ).get_as_text()
-	D1 = JSON.parse_string(dia_text)
+	var dia_text_file = FileAccess.open("Scenes/Dialogues/D1.txt", FileAccess.READ)
+	D1 = JSON.parse_string(dia_text_file.get_as_text())
+	dia_text_file.close()
 
-	dia_text = FileAccess.open("Scenes/Dialogues/D2.txt", FileAccess.READ).get_as_text()
-	D2 = JSON.parse_string(dia_text)
+	dia_text_file = FileAccess.open("Scenes/Dialogues/D2.txt", FileAccess.READ)
+	D2 = JSON.parse_string(dia_text_file.get_as_text())
+	dia_text_file.close()
--- Scripts/Root.gd
+++ Scripts/Root.gd
@@ -1,13 +1,47 @@
 extends Node2D
+var firefly_packed = preload("res://Scenes/Firefly.tscn")
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		#get_tree().quit()
 		add_child(preload("res://Scenes/PauseMenu.tscn").instantiate())
 		get_tree().paused = true
+	if Input.is_action_just_pressed("ui_page_up"):
+		save_game()
+	if Input.is_action_just_pressed("ui_page_down"):
+		load_game()
 
 func _physics_process(_delta):
 	var to_translate = $Player.position
 	if to_translate.y > -256:
 		to_translate.y = -256
 	$Camera2D.translate(to_translate - $Camera2D.position)
+
+
+func save_game():
+	print("saving")
+	var f_loc_arr = []
+	for f in $FireflyContainer.get_children():
+		f_loc_arr.append([f.position.x, f.position.y, f.is_big])
+	print(f_loc_arr)
+	var save_file = FileAccess.open("Scenes/SaveFile.txt", FileAccess.WRITE)
+	save_file.store_string(JSON.stringify(f_loc_arr))
+	save_file.close()
+
+
+func load_game():
+	print("loading")
+	for f in $FireflyContainer.get_children():
+		f.queue_free()
+	var f_loc_arr = []
+	var save_file = FileAccess.open("Scenes/SaveFile.txt", FileAccess.READ)
+	f_loc_arr = JSON.parse_string(save_file.get_as_text())
+	print(f_loc_arr)
+	for f_loc in f_loc_arr:
+		var firefly = firefly_packed.instantiate()
+		firefly.position = Vector2(f_loc[0], f_loc[1])
+		firefly.is_big = f_loc[2]
+		if firefly.is_big:
+			firefly.scale = Vector2(2.5, 2.5)
+		$FireflyContainer.add_child(firefly)
